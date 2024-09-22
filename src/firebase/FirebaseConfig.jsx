7871d8fc0953175b3f033bbe0c9e4bb3a448d45b
Rahmat Ali <147299931+RahmatAli-15@users.noreply.github.com>
2024-09-22 21:25:33 +0530

    Update FirebaseConfig.jsx

--- src/firebase/FirebaseConfig.jsx
+++ src/firebase/FirebaseConfig.jsx
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA8ZV7skwmRSOq8sQOCWGJQpjCZF7CkKO0",
+  apiKey: "",
   authDomain: "jwellery-e0f90.firebaseapp.com",
   projectId: "jwellery-e0f90",
   storageBucket: "jwellery-e0f90.appspot.com",
-  messagingSenderId: "853248345371",
-  appId: "1:853248345371:web:37d0d20a4e23eff3e9ffbe"
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
